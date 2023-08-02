c91fe6de55974a7cd348e2e2a8bcb1ceaa8f4cb6
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-08-02 20:21:18 +0900

    today

--- init/target/debug/deps/init-09391b9c25898b06.d
+++
@@ -1,5 +0,0 @@
-/home/kiririn/git/atcoder/init/target/debug/deps/init-09391b9c25898b06.rmeta: src/main.rs
-
-/home/kiririn/git/atcoder/init/target/debug/deps/init-09391b9c25898b06.d: src/main.rs
-
-src/main.rs:
--- init/target/debug/deps/init-93931b28539d0394.d
+++
@@ -1,5 +0,0 @@
-/home/kiririn/git/atcoder/init/target/debug/deps/init-93931b28539d0394.rmeta: src/main.rs
-
-/home/kiririn/git/atcoder/init/target/debug/deps/init-93931b28539d0394.d: src/main.rs
-
-src/main.rs:
