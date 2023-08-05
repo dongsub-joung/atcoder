71f0fb0de66ec82a3f3dc2cc851fe601fdd537b5
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-08-06 04:32:54 +0900

    update

---
+++ ABC313/A-2.py
@@ -0,0 +1,19 @@
+import sys
+
+input= sys.stdin.readline
+
+N= int(input())
+P_arr= list(map(int,input().split()))
+answer= 0
+
+max_value= max(P_arr)
+first= P_arr.pop(0)
+if max_value == first:
+    print(0)
+    exit(1)
+
+max_v= max(P_arr)
+
+answer= max_v-first+1
+
+print(answer)
---
+++ ABC313/A.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList nums=new ArrayList();
+        Scanner sc=new Scanner(System.in);
+
+        var N=sc.nextInt();
+        for(int i=1;i<=N;i++){
+            nums.add(sc.nextInt());
+        }
+        Integer all_max= (Integer) Collections.max(nums);
+        if(nums.get(0) ==all_max){
+            System.out.println(0);
+            System.exit(0);
+        }else{
+            var answer= ((int) all_max) - nums.get(0) + 1;
+            System.out.println(answer);
+    }
+}
