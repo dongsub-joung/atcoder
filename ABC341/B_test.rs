5a8d4e7234e9ff6e644569704bb32562dc052e4b
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-02-19 21:43:11 +0900

    341 B test2 err

--- ABC341/B_test.rs
+++ ABC341/B_test.rs
@@ -1,7 +1,19 @@
-pub fn solution(n: usize, a_vec: Vec<usize>, st: Vec<(usize, usize)>) -> usize {
-    
+pub fn solution(n: usize, mut a_vec: Vec<usize>, st: Vec<(usize, usize)>) -> usize {
+    loop {
+        let max= a_vec.iter().max().unwrap().clone();
+        let max_index= a_vec.iter().position(|&f| f == max).unwrap();
+        let i= max_index+1;
 
-    0_usize   
+        let (s,t)= st[i];
+        if a_vec[max_index] > s {
+            a_vec[max_index] -= s;
+            a_vec[max_index+1] += t;
+        }else{
+            break;
+        }
+    }
+    
+    a_vec.iter().max().unwrap().clone()
 }
 
 #[cfg(test)]
@@ -22,10 +34,8 @@ mod tests {
     fn it_works2() {
         let n= 10;
         let a_vec= Vec::from([32, 6, 46, 9, 37, 8, 33, 14, 31,5]);
-        let st_vec= Vec::from([(5,5), (4,3), (2,2), (3,2), (3,2), (4,4), (3,3), (3,1)]);
-
+        let st_vec= Vec::from([(5,5), (3,1), (4,3), (2,2), (3,2), (3,2), (4,4), (3,3), (3,1)]);
         let result = solution(n, a_vec, st_vec);
         assert_eq!(result, 45);
     }
 }
-
