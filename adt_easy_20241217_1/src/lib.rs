4f1bfd803c0eff4e65c9497d0a1146287243d4c5
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-12-18 19:08:00 +0900

    done but something wrong

--- adt_easy_20241217_1/src/lib.rs
+++ adt_easy_20241217_1/src/lib.rs
@@ -26,7 +26,31 @@ mod tests {
 // https://atcoder.jp/contests/adt_easy_20241217_1/tasks/abc304_a
 pub fn solution_b(n: i32, s_v: Vec<(&str, i32)>) -> Vec<&str> {
     let mut result_v= Vec::new();
+    let min_age= s_v.iter().min().unwrap().1;
+    let mut ages: Vec<i32>= Vec::new();
 
+    for (k,v) in s_v.clone() {
+        ages.push(v);
+    }
+
+    let min= ages.iter().min().unwrap();
+
+    let mut idx= 0;
+    for (i,(k, v)) in s_v.clone().iter().enumerate(){
+        if min == v{
+            idx= i;
+        }
+    }
+
+    for i in idx..(n as usize){
+        let (k, v)= s_v[i];
+        result_v.push(k);
+    }
+
+    for i in 0..idx{
+        let (k, v)= s_v[i];
+        result_v.push(k);
+    }
     
     result_v
 }
--- adt_easy_20241217_1/src/main.rs
+++ adt_easy_20241217_1/src/main.rs
@@ -10,10 +10,56 @@ let c= char::from_u32(n).unwrap();
 println!("{}", c.to_string());    
 }
 
+// https://atcoder.jp/contests/adt_easy_20241217_1/tasks/abc304_a
+pub fn solution_b(){
+    let mut result_v= Vec::new();
+    let mut s_v: Vec<Vec<&str>>= Vec::new();
+
+    let std= stdin();
+    let mut buf= std.lock().lines();
+    let n= buf.next().unwrap().unwrap()
+    .parse::<u32>().unwrap();
+
+    for _ in 0..n {
+        let person: Vec<&str>= buf.next().unwrap().unwrap().clone()
+            .split_whitespace()
+            .collect();
+        s_v.push(person);
+    }
+    
+    let mut ages: Vec<i32>= Vec::new();
+
+    for v in s_v.clone() {
+        ages.push(v[1].parse::<i32>().unwrap());
+    }
+
+    let min= ages.iter().min().unwrap();
+
+    let mut idx= 0;
+    for (i, v) in s_v.clone().iter().enumerate(){
+        let v= v[1].parse::<i32>().unwrap();
+        if *min == v{
+            idx= i;
+        }
+    }
+
+    for i in idx..(n as usize){
+        result_v.push(s_v[i][1]);
+    }
+
+    for i in 0..idx{
+        result_v.push(s_v[i][1]);
+    }
+    
+    for v in result_v {
+        println!("{}", v);
+    }
+}
+
 fn main(){
     // https://atcoder.jp/contests/adt_easy_20241217_1/tasks/abc252_a
-    solution_a();
+    // solution_a();
 
     // https://atcoder.jp/contests/adt_easy_20241217_1/tasks/abc304_a
-    
+    solution_b();
 }
