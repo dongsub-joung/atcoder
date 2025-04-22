8a5213d7b8fedb2a171bf2ab8f752cf4ad7aeecc
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-04-22 21:41:05 +0900

    2025-04-22 21:38:01	A - Chord	kizari 	Zig (Zig 0.10.1)

---
+++ adt_easy_20250422_1/q_A.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
+
+pub fn main() !void {
+    var buffer: [10]u8 = undefined;
+    @memset(buffer[0..], 0);
+    _ = try stdin.readUntilDelimiterOrEof(buffer[0..], '\n');
+
+    if (std.mem.eql(buffer, "ACE")) {
+        try stdout.print("Yes");
+    }
+    if (std.mem.eql(buffer, "BDF")) {
+        try stdout.print("Yes");
+    }
+    if (std.mem.eql(buffer, "CEG")) {
+        try stdout.print("Yes");
+    }
+    if (std.mem.eql(buffer, "DFA")) {
+        try stdout.print("Yes");
+    }
+    if (std.mem.eql(buffer, "EGB")) {
+        try stdout.print("Yes");
+    }
+    if (std.mem.eql(buffer, "FAC")) {
+        try stdout.print("Yes");
+    }
+    if (std.mem.eql(buffer, "GBD")) {
+        try stdout.print("Yes");
+    }
+
+    try stdout.print("No")
+}
