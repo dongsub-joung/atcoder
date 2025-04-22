d9a84db3ce8db274512a5cbc8e33f91523b55fac
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-04-23 06:08:45 +0900

    2025-04-23 05:57:34A - Chordkizari Kotlin (Kotlin/JVM 1.8.20)

--- adt_easy_20250422_1/q_A.kt
+++ adt_easy_20250422_1/q_A.kt
@@ -1,1 +1,17 @@
-cc
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    var line= sc.nextLine()
+
+    if (line.contains("ACE")
+        || line.contains("BDF")
+        || line.contains("CEG")
+        || line.contains("DFA")
+        || line.contains("EGB")
+        || line.contains("FAC")
+        || line.contains("GBD"))
+        println("Yes")
+    else
+        println("No")
+}
